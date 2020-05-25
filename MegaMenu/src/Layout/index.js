209abd566092289fab76a0db81bcffe2e9593561
NguyetMoon <nguyetnguyetrose9898@gmail.com>
2020-05-25 02:43:57 +0000

    update footer

--- MegaMenu/src/Layout/index.js
+++ MegaMenu/src/Layout/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Navbar from '../Component/Navigation';
+import Footer from '../Component/Footer';
 function Layout ({children}) {
     return (
        <div>
            <Navbar/>
            {children}
+           <Footer/>
        </div>
     )
 }
